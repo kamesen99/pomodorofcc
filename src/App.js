e955cc80445f0ed3b47c7845397e8cd78f73728e
miguelpascual <miguel@yonder.com>
2020-11-18 23:17:40 -0800

    Fixed decrement timer. Thanks Darren. Added functionality for timer === 0. Up and down arrows change the timer if that type is active.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState} from 'react';
 import {AccurateInterval} from "./components/AccurateInterval"
 
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ReplayIcon from '@material-ui/icons/Replay';
-import Counter from "./components/Counter"
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import StopIcon from '@material-ui/icons/Stop';
+
 import './App.css';
 
 function App() {
@@ -17,45 +15,33 @@ function App() {
   const [timerState, setTimerState] = useState("stopped");
   const [timerType, setTimerType] = useState("Session");
   const [timerID, setTimerID] = useState("");
-  const [timer, setTimer] = useState(25*60);
-  //const [reset, setReset] = useState(false);
+  const [timer, setTimer] = useState(2);
   
-  const handleClickDown = (timer, setFunc) =>{
+  const useAudio = new Audio("BeepSound.wav");
+
+  const handleClickDown = (timer, type, setFunc) =>{
     if (timer > 1) {
       setFunc(timer - 1);    
+      if (timerType === type) {
+        setTimer(timer*60 - 60);
+      } 
     }
   }
-  const handleClickUp = (timer, setFunc) =>{
+  const handleClickUp = (timer, type, setFunc) =>{
     if (timer < 60) {
         setFunc(timer + 1);
+        if (timerType === type) {
+          setTimer(timer*60 + 60);
+        } 
     }
   }
-  const handleStartStop = () => {
-    // setTimerStopped(!timerStopped);
-    console.log("Play/pause pressed!")
-  }
   
-  const startBeep = () => {
-    const useAudio = new Audio("BeepSound.wav");
-    useAudio.play();
-  }
-
-  // const countDown = () =>{
-  //   setTimeout(() => setTimer(timer - 1), 1000)
-  // }
-  // React.useEffect(() => {
-  //   //timer > 0 && (!timerStopped && countDown());
-  // })
-
   const TimeControl = () => {
-    console.log("Timer Control!")
     if (timerState === "stopped"){
       startCountDown();
       setTimerState("running");
-      console.log("Running");
     } else {
       setTimerState("stopped")
-      console.log("Stopped")
       if (timerID) {
         timerID.cancel();
       }
@@ -63,47 +49,72 @@ function App() {
   }
   
   const startCountDown = () => {
-    console.log("Countdown started!")
-    setTimerID(AccurateInterval(() => {
+    console.log("Countdown started!") 
+    const timerId = AccurateInterval(() => {
       decrementTimer();
-    }, 1000))
+      phaseControl();
+    }, 1000);
+    setTimerID(timerId);
   }
-
+  
   const decrementTimer = () => {
     console.log("Inside Decrement Timer")
-    setTimer(timer - 1); 
-    console.log("Timer State: " + timer);
+    setTimer(timer => timer - 1);
   }
 
-
-  const handleEnd = () => {
-    if (timer === 0) {
-      startBeep();
+  const switchSessionType = (time, type) => {
+    setTimer(time);
+    setTimerType(type);
+  }
+  
+  //Not working. Cannot get the timer < 0 test to fail
+  const phaseControl = () => {
+    console.log("Phase Control")
+    //Cannot get timer === 0 test to fail
+    handleEnd();
+    if (timer < 0) {
+      if (timerID) {
+        timerID.cancel();
+      }
+      if (timerType === "Break") {
+        startCountDown();
+        switchSessionType(breakTime*60, "Break");
+      } else {
+        startCountDown();
+        console.log("Timer Type!")
+        switchSessionType(sessionTime*60, "Session");
+      }
     }
   }
-
+  
+  const handleEnd = () => {
+    //Unable to access "timer" state?
+    if (timer === 0) {
+      console.log("Handle end success!")
+      useAudio.play();
+    }
+  }
+  
   const handleReset = () => {
     setBreakTime(5);
     setSessionTime(25);
     setTimerState("stopped");
     setTimerType("Session");
     setTimerID("");
-    setTimer(25*60);
+    setTimer(3);
+    useAudio.pause();
+    useAudio.currentTime = 0;
   }
 
-  const Counter = (timer) => {
-    //const [timeleft, setTimeleft] = useState(timer * 60);
-    
-    const timeToFormat = (minutes, seconds) => {
+  const FormatTime = (timer) => {
+    const numberToTime = (minutes, seconds) => {
       minutes = minutes < 10 ? "0" + minutes : minutes;
       seconds = seconds < 10 ? "0" + seconds : seconds;
       return (minutes + ":" + seconds);
     }
-
     let minutes = Math.floor(timer / 60);
     let seconds = timer % 60;
-    let formatted = (timeToFormat(minutes, seconds))
-
+    let formatted = (numberToTime(minutes, seconds))
     return formatted;
   }
 
@@ -115,14 +126,14 @@ function App() {
       <div className="App-body">
         <div className="section">
           <div id="break-label">
-            <ArrowDownwardIcon id="break-decrement" onClick={() => handleClickDown(breakTime, setBreakTime)} style={{marginRight: "1rem"}}/>
-              <p id="break-length" >{breakTime}</p>:00
-            <ArrowUpwardIcon id="break-increment" onClick={() => handleClickUp(breakTime, setBreakTime)} style={{marginLeft: "1rem"}}/>
+            <ArrowDownwardIcon id="break-decrement" onClick={() => handleClickDown(breakTime, "Break", setBreakTime)} style={{marginRight: "1rem"}}/>
+              <p id="break-length" >{FormatTime(breakTime*60)}</p>
+            <ArrowUpwardIcon id="break-increment" onClick={() => handleClickUp(breakTime, "Break", setBreakTime)} style={{marginLeft: "1rem"}}/>
           </div>
           <div id="session-label">
-            <ArrowDownwardIcon id="session-decrement" onClick={() => handleClickDown(sessionTime, setSessionTime)} style={{marginRight: "1rem"}}/>
-              <p id="session-length" >{sessionTime}</p>:00
-            <ArrowUpwardIcon id="session-increment" onClick={() => handleClickUp(sessionTime, setSessionTime)} style={{marginLeft: "1rem"}}/>
+            <ArrowDownwardIcon id="session-decrement" onClick={() => handleClickDown(sessionTime, "Session", setSessionTime)} style={{marginRight: "1rem"}}/>
+              <p id="session-length" >{FormatTime(sessionTime*60)}</p>
+            <ArrowUpwardIcon id="session-increment" onClick={() => handleClickUp(sessionTime, "Session", setSessionTime)} style={{marginLeft: "1rem"}}/>
           </div>
         </div> 
         <div className="timer-section">
@@ -131,7 +142,7 @@ function App() {
             <ReplayIcon id="reset" onClick={handleReset} />
           </div>
           <div>
-          Session: <div id="timer-label">{timerType}</div> <div id="time-left">{Counter(timer)}</div>
+          Session: <div id="timer-label">{timerType}</div> <div id="time-left">{FormatTime(timer)}</div>
           </div>          
         </div>
       </div>
--- src/components/AccurateInterval.js
+++ src/components/AccurateInterval.js
@@ -3,7 +3,6 @@
 export const AccurateInterval = (fn, time) => {
       var cancel, nextAt, timeout, wrapper;
       nextAt = new Date().getTime() + time;
-      console.log("Accurate Interval!")
       timeout = null;
       wrapper = function() {
         nextAt += time;
